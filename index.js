79a8f62ab9c9a53809ba509ee8a329def61ef72d
Wei <catch0822@gmail.com>
2017-06-02 22:31:41 +0800

    測試星座運勢

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var linebot = require('linebot');
 var express = require('express');
+var fetchUrl = require("fetch").fetchUrl;
 
 var bot = linebot({
     channelId: '1518123694',
@@ -7,8 +8,27 @@ var bot = linebot({
     channelAccessToken: 'QKCZBLlIhDQhShlwWfO6+ZlrXIjaLxeo/c9+z2lMECIl3/LSD1f4sdQfg/gWPZkIESMmlmS0vKY9YRYxaG2kiOBBKl6gHxXGABs0N6IQEhjOLrenmxRZb49D8z7GbL0Qjgoifx4mQSdQ61QI4kn8swdB04t89/1O/w1cDnyilFU='
 });
 
+function getContent(url, cb){
+    var request = require("request");
+    request(url, function (error, response, body) {
+        var str = body.toString().replace(/\r\n|\n/g,"").replace(/\s+/g, "");
+        var match, result = "", regex = /<article>(.*?)<\/article>/ig;
+        while (match = regex.exec(str)) { result += match[1]; }
+        cb(result)
+    });
+}
+
 bot.on('message', function (event) {
     console.log(event); 
+    if(event.message.text.indexOf("8363")){
+        if(event.message.text.indexOf("牡羊座") > -1){
+            self = event;
+            getContent("http://www.daily-zodiac.com/mobile/zodiac/Aries", function(res){
+                self.reply("牡羊座今日運勢:"+ res);
+            });
+        }
+    }
+
     if(event.message.text.indexOf("涵涵") > -1) {
         event.reply("83最愛涵涵了")
     }
@@ -32,8 +52,9 @@ const linebotParser = bot.parser();
 app.post('/linewebhook', linebotParser);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+    res.send('Hello World!');
+  });
+
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
