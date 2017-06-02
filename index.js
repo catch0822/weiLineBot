6e7f43a6b84c87b7b796d210376fcfe3c100e852
Wei Chang <wei_chang@wistron.com>
2017-06-02 15:56:39 +0800

    bug fixed

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ bot.on('message', function (event) {
     });
 });
 
+bot.listen('/linewebhook', process.env.PORT || 8080);
+
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post('/linewebhook', linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
